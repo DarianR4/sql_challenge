c2a132d46708aff390b6e885ad9055f24669be51
DarianR4 <79239254+DarianR4@users.noreply.github.com>
2021-05-14 00:19:18 -0400

    second push
    using data site for table creation take 1

---
+++ EmployeeSQL/create_tables.sql
@@ -0,0 +1,43 @@
+
+CREATE TABLE `employees` (
+    `emp_no` int  NOT NULL ,
+    `emp_title_id` int  NOT NULL ,
+    `birth_date` date  NOT NULL ,
+    `first_name` varchar(200)  NOT NULL ,
+    `last_name` varchar(200)  NOT NULL ,
+    `sex` string  NOT NULL ,
+    `hire_date` date  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+CREATE TABLE `salaries` (
+    `emp_no` int  NOT NULL ,
+    `salary` money  NOT NULL ,
+    PRIMARY KEY (
+        `emp_no`
+    )
+);
+
+CREATE TABLE `titles` (
+    `title_id` int  NOT NULL ,
+    `title` varchar(200)  NOT NULL 
+);
+
+CREATE TABLE `departments` (
+    `dept_no` int  NOT NULL ,
+    `dept_name` varchar(200)  NOT NULL 
+);
+
+CREATE TABLE `dept_emp` (
+    `emp_no` int  NOT NULL ,
+    `dept_no` int  NOT NULL 
+);
+
+CREATE TABLE `debt_manager` (
+    `dept_no` int  NOT NULL ,
+    `emp_no` int  NOT NULL 
+);
+
+
