25d7d854d50f884be77a07150543ad9ae14a4d6b
DarianR4 <79239254+DarianR4@users.noreply.github.com>
2021-05-17 23:22:22 -0400

    updated table commands

--- EmployeeSQL/create_tables.sql
+++ EmployeeSQL/create_tables.sql
@@ -1,43 +1,44 @@
-
-CREATE TABLE `employees` (
-    `emp_no` int  NOT NULL ,
-    `emp_title_id` int  NOT NULL ,
-    `birth_date` date  NOT NULL ,
-    `first_name` varchar(200)  NOT NULL ,
-    `last_name` varchar(200)  NOT NULL ,
-    `sex` string  NOT NULL ,
-    `hire_date` date  NOT NULL ,
+CREATE TABLE employees (
+    emp_no int  NOT NULL ,
+    emp_title_id int  NOT NULL ,
+    birth_date date  NOT NULL ,
+    first_name varchar(200)  NOT NULL ,
+    last_name varchar(200)  NOT NULL ,
+    sex string  NOT NULL ,
+    hire_date date  NOT NULL ,
     PRIMARY KEY (
-        `emp_no`
+        emp_no
     )
 );
 
-CREATE TABLE `salaries` (
-    `emp_no` int  NOT NULL ,
-    `salary` money  NOT NULL ,
+CREATE TABLE salaries (
+    emp_no int  NOT NULL ,
+    salary money  NOT NULL ,
     PRIMARY KEY (
-        `emp_no`
+        emp_no
     )
 );
 
-CREATE TABLE `titles` (
-    `title_id` int  NOT NULL ,
-    `title` varchar(200)  NOT NULL 
+CREATE TABLE titles (
+    title_id int  NOT NULL ,
+    title varchar(200)  NOT NULL 
 );
 
-CREATE TABLE `departments` (
-    `dept_no` int  NOT NULL ,
-    `dept_name` varchar(200)  NOT NULL 
+CREATE TABLE departments (
+    dept_no int  NOT NULL ,
+    dept_name varchar(200)  NOT NULL 
 );
 
-CREATE TABLE `dept_emp` (
-    `emp_no` int  NOT NULL ,
-    `dept_no` int  NOT NULL 
+CREATE TABLE dept_emp (
+    emp_no int  NOT NULL ,
+    dept_no int  NOT NULL 
 );
 
-CREATE TABLE `debt_manager` (
-    `dept_no` int  NOT NULL ,
-    `emp_no` int  NOT NULL 
+CREATE TABLE debt_manager (
+    dept_no int  NOT NULL ,
+    emp_no int  NOT NULL 
 );
 
+CREATE INDEX idx_employees_sex
+ON employees (sex);
 
