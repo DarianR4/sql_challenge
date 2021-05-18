18789db12ac3d28f9e61488033956daf4183d94d
DarianR4 <79239254+DarianR4@users.noreply.github.com>
2021-05-18 02:50:53 -0400

    all queries done

--- EmployeeSQL/db_queries.sql
+++ EmployeeSQL/db_queries.sql
@@ -56,5 +56,8 @@ departments.dept_name = 'Development'
 ;
 
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
-select last_name, first_name, sext
+SELECT last_name, COUNT(last_name) AS "ln_frequency count"
+FROM employees
+GROUP BY last_name
+ORDER BY "ln_frequency count" DESC;
 
